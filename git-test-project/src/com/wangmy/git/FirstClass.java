45e587dc99e3c4b99b156e551823594694dda8f1
wangmy <568704029@qq.com>
2019-11-23 15:00:55 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/wangmy/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.wangmy.git;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by wangmy on 2019/11/23.
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+        System.out.println("hello project");
+    }
+}
